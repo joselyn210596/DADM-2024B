98cb960008b2a4a0198519353d42eb18593d5f44
joselyn210596 <159564499+joselyn210596@users.noreply.github.com>
2024-10-02 17:47:18 +0000

    ⭐S11 Methods Add method to add item and clean input

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,58 @@
 <script setup>
-import{ref} from 'vue';
+import { ref } from 'vue';
+// Modelo
 const header = ref('App lista de compras');
+//-----items-------
+// item-Model
 const items = ref([
-    {id:'0', label: '10 bolillos'},
-    {id:'1', label: '1 lata de frijoles'},
-    {id:'2', label: '1 nutella'},
-    {id:'3', label: '1 chela'}
-    
+  {id:'0', label: '10 bolillos'},
+  {id:'1', label: '1 crema de cuerpo'},
+  {id:'2', label: '1 chela'},
+  {id:'3', label: '1 nutella'}
 ]);
-const newItem =ref('');
-const newItemPriority= ref('low');
+//Item-Method
+const saveItem = () =>{
+  // Add new item
+  items.value.push({id:items.value.length+ 1, label: newItem.value});
+  // limpia el input
+  newItem.value='';
+};
+const newItem = ref("");
+const newItemHighPriority = ref(false);
+
+//Metodos
 </script>
 
 <template>
-<h1> 
-    <i class="material-icons shopping-cart-icon">local_mall </i>
-    {{ header }}
-</h1>
-<input v-model="newItem" type="text"placeholder="Agregar Articulo">
-<!-- RADIO BUTTOS-->
- <label>
-    <input type="radio" value="low" v-model="newItemPriority">
-    Bajo
-</label>
- <label>
-    <input type="radio" value="high"  v-model="newItemPriority">
-    Alta
-</label>
-{{ newItemPriority == 'low' ? '🍧': '🔥'}}
-<ul>
- <li v-for="(item, i) in items" :key="item.id"> {{ i+1 }} {{ i%2==0?'🔥':'🛍' }} {{ item.label }}</li>
-</ul>
+  <h1>
+   <i class="material-icons shopping-cart-icon">local_mall</i> 
+    {{ header }} 
+  </h1>
+  <form class="add-item form" v-on:submit.prevent="saveItem()">
+  <input v-model="newItem" type="text" placeholder="Agregar un articulo" />
+  <!--Caja de seleccion de Prioridad-->
+  <label>
+    <input type="checkbox" v-model="newItemHighPriority" />
+    Alta Prioridad
+  </label>
+  <!--Boton-->
+  <button class="btn btn-primary">
+    Salvar Articulo
+  </button>
+  </form>
+  <ul></ul>
+ 
+  {{ iceCreamFlavors }}
+  <ul></ul>
+  {{ newItemHighPriority }}
+  <!-- Lista -->
+  <ul>
+    <li v-for="item in items" :key="item.id"> 👝 {{  item.label }} </li>
+  </ul>
 </template>
 
 <style scoped>
-.shopping-cart-icon {
- font-size: 2rem; 
+.shopping-cart-icon{
+  font-size: 2rem;
 }
 </style>
