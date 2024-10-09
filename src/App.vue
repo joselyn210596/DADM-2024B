ce127dc0e3378ca64b1f7223c76b6e2238b23dc2
joselyn210596 <159564499+joselyn210596@users.noreply.github.com>
2024-10-09 17:10:42 +0000

    Enlace Dinamico

--- src/App.vue
+++ src/App.vue
@@ -2,54 +2,68 @@
 import { ref } from 'vue';
 // Modelo
 const header = ref('App lista de compras');
-//-----items-------
-// item-Model
+//---items---
+//items model
 const items = ref([
   {id:'0', label: '10 bolillos'},
-  {id:'1', label: '1 crema de cuerpo'},
-  {id:'2', label: '1 chela'},
+  {id:'1', label: '1 crema de litro'},
+  {id:'2', label: '1/4 de jamon'},
   {id:'3', label: '1 nutella'}
 ]);
-//Item-Method
-const saveItem = () =>{
-  // Add new item
-  items.value.push({id:items.value.length+ 1, label: newItem.value});
-  // limpia el input
+//item-metodo
+const seveItem = () => {
+  //accediendo a la variable reactiva items value.
+  items.value.push({id:items.value.length + 1, label: newItem.value});
+  //limpia el imput 
   newItem.value='';
 };
-//-- Formulario -----
-const newItem = ref("");
+//---formulario--
+const newItem = ref('');
 const newItemHighPriority = ref(false);
-const editing= ref(true);
-const activateEdit = (activate) => {
-  editing.value = activate;
+const editing = ref(true);
+const activateEdition = (activate)=>{
+  editing.value=activate;
 };
-
-//Metodos
+//metodos 
 </script>
 
 <template>
-  <div class="header">
+  
+<div class="header">
   <h1>
-  <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }} 
+   <i class="material-icons shopping-cart-icon">local_mall</i> 
+    {{ header }} 
   </h1>
-  <button 
-  v-if="editing" 
-  class="btn" 
-  @click="activateEdit(false)">
-  Cancelar
-</button>
-  <button 
-  v-else
-   class="btn btn-primary"
-    @click="activateEdit(true)">
-    Agregar Articulo 
+  <button
+   v-if="editing" 
+   class="btn"
+    @click="activateEdition(false)">
+    Cancelar
+  </button>
+  <button
+   v-else
+    class="btn btn-primary" 
+    @click="activateEdition(true)">
+    Agregar Articulo
   </button>
   </div>
-  
-  <form class="add-item form"
+
+  <!---colocando un iperenlace-->
+   <!---para poder poner link enla caja de texto v-bind:href="newItem"-->
+     <!--para que cuando este bacia la caja de texto tenga un link
+     :href="newItem === '' ? 'https://www.google.com' : 'https://' + newItem" 
+    target="_blank" -->
+ 
+  <a 
+    :href="newItem === '' ? 'https://www.google.com' : 'https://' + newItem" 
+    target="_blank">
+    {{ newItem === '' ? '😒 Link' : newItem }}
+  </a>
+  <!--- agrupando entradas de usuario-->
+  <form 
+  class="add-item form"
   v-if="editing"
-   v-on:submit.prevent="saveItem()">
+   v-on:submit.prevent="seveItem">
   <input v-model="newItem" type="text" placeholder="Agregar un articulo" />
   <!--Caja de seleccion de Prioridad-->
   <label>
@@ -62,15 +76,15 @@ const activateEdit = (activate) => {
   </button>
   </form>
   <ul></ul>
- 
+
   {{ iceCreamFlavors }}
   <ul></ul>
   {{ newItemHighPriority }}
   <!-- Lista -->
   <ul>
-    <li v-for="item in items" :key="item.id"> 👝 {{  item.label }} </li>
+    <li v-for="item in items" :key="item.id"> 🛍 {{  item.label }} </li>
   </ul>
-  <p v-if="items.length === 0">🥀 NO HAY ELEMENTOS EN TU LISTA 🥀</p>
+  <p V-if="items.length === 0">🥀NO HAY ELEMNTOS EN LISTA🥀 </p>
 </template>
 
 <style scoped>
