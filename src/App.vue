be5fd7e1cce9f0c6d585cf84ba0f48cd80392489
joselyn210596 <159564499+joselyn210596@users.noreply.github.com>
2024-11-06 18:45:16 +0000

    ⭐S03S03 PROPERTIES

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
+import {ref} from 'vue';
 import planItem from './components/plan-item.vue';
+const plans= ref(['El cafetero','El chelero','El intelectual']);
 </script>
 
 <template>
@@ -18,9 +20,8 @@ import planItem from './components/plan-item.vue';
     </h2>
 
     <div class="plans">
-      <planItem></planItem>
-      <planItem></planItem>
-      <planItem></planItem>
+      <planItem v-for="plan in plans":key="plan" :name="plan"/>
+     
     </div>
   </div>
 </template>
--- src/components/plan-item.vue
+++ src/components/plan-item.vue
@@ -2,14 +2,16 @@
     <div class="plan">
       <div class="description">
         <span class="title">
-          El adicto
+          {{ name }} 
         </span>
       </div>
     </div>
   </template>
   
   <script setup>
-  
+  //Usando un macro para definir propiedades
+   //defineProps(['name','planIcon']);
+   defineProps({name:{type: String, required: true} });
   </script>
   
   <style scoped>
