0db2d77c7e9de8b840cd624f18e75d7ab513d881
joselyn210596 <159564499+joselyn210596@users.noreply.github.com>
2024-11-11 18:51:32 +0000

    S03S05 Eventos

--- src/components/planpicker.vue
+++ src/components/planpicker.vue
@@ -2,14 +2,15 @@
 import PlanpickerItem from './planpickerItem.vue';
 import planpickerItem from './planpickerItem.vue';
 import {ref} from 'vue';
-const plans= ref(['El cafetero','El chelero','El intelectual']);      
+const plans= ref(['El cafetero','El chelero','El intelectual']); 
+const selectPlan=(name)=>{console.log(name)};     
 </script>
 
 <template>
     <div class="plans">
     <PlanpickerItem v-for="plan in plans"
     :key="plan" :name="plan" 
-    @select="(name) =>{console.log(name)}"/>
+    @select="selectPlan"/>
      
     </div>
 </template>
