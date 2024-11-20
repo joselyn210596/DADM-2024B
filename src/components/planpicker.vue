c04f4cbda4b2a807fd05189579df1eecf9942440
joselyn210596 <159564499+joselyn210596@users.noreply.github.com>
2024-11-20 18:50:55 +0000

    🚧S03S06 Hooks

--- src/components/planpicker.vue
+++ src/components/planpicker.vue
@@ -1,14 +1,21 @@
 <script setup>
 import PlanpickerItem from './planpickerItem.vue';
 import planpickerItem from './planpickerItem.vue';
-import {ref} from 'vue';
+import {ref, onMounted} from 'vue';
 // creando una referenci creativa para almacenar el plan seleccionado
 const selectedPlan = ref(null);
 const plans= ref(['El cafetero','El chelero','El intelectual']); 
 const selectPlan=(name)=>{
 // almacenar el plan seleccionado
 selectedPlan.value = name;
-};    
+};  
+//Intentando obtener la referencia al elemento .plans
+console.log("termina de ejecutar el script setup 😅");
+//registrando el hook mounted
+onMounted(() => {console.log("el componente PlanPicker se ha montado");
+    console.log(document.querySelector('.plans'));
+ });
+
 </script>
 
 <template>
